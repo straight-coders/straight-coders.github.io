06b0436386062a1e2535140cfadb453d461c0b41
Rico Monden <mond0040@algonqinlive.com>
2019-04-15 10:45:39 -0400

    Set equal height and add responsive to option card

--- js/home.js
+++ js/home.js
@@ -1,11 +1,11 @@
 
+$(document).ready(function () {
+  $('.bounce').click(function () {
+    console.log('works')
+    $(this).effect('bounce', {times: 2}, 1000)
+  })
+})
 
-$(document).ready(function() {
-
-    $(".bounce").click(function(){
-        console.log('works');
-       $(this).effect( "bounce", {times:2}, 1000 );
-    });
-
- });
-
+$(function () {
+  $('.option-card').matchHeight()
+})
